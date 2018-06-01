2ea48ece77503d34d1c19e999d1df43a96beffc7
Felipe Henao <doneright@ool-ad0201bf.dyn.optonline.net>
2018-06-01 16:25:22 -0400

    workig on the Todo component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Todo from './Components/Todo';
 import Navbar from './Components/Header';
@@ -7,11 +7,34 @@ import Footer from './Components/Footer';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: []
+    }
+  }
+  fetchTasks() {
+    fetch('/task')
+      .then(resp => {
+        if (!resp.ok) {
+          throw Error('failure: ', resp.message);
+        }
+        return resp.json();
+      }).then(data => this.setState ({
+        tasks: data.data
+      })).catch(err => console.log(`error: ${err}`))
+  }
+
+  componentDidMount() {
+    this.fetchTasks();
+  }
   render() {
     return (
       <div className="App">
         <Navbar/>
         <p>App.js</p>
+        <Todo/>
         <Footer/>
       </div>
     );
--- client/src/Components/Todo.js
+++ client/src/Components/Todo.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Todo extends Component {
+	render() {
+		return(
+			<div>
+				<h2>Todo</h2>
+
+				{/* {this.props.todo.map(task => (
+
+					<div Key={todo.id}
+						className='todo-list'>
+						<div key={todo.id}>
+							{todo.id}
+							<p>{todo.task}</p>
+							<Link to ={`todo/${task.id}`}>Edit task</Link>
+						</div>
+					</div>
+				))} */}
+			</div>
+		)
+	}
 }
 
 export default Todo;
