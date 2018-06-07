abda95b2359a840de87b89a70449a5aac2db9338
Felipe <felhenao@gmail.com>
2018-06-07 17:01:58 -0400

    added nav bar

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
 	render() {
 		return(
-			<h2></h2>
+			<nav className="nav-bar">
+				<Link to='/api/todo'>
+					<li className="nav-li">Todo</li>
+				</Link>
+				<Link to='/api/todo/new'>
+					<li className="nav-li">New</li>
+				</Link>
+			</nav>
 		)
 	}
 }
